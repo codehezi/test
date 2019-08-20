0763d8c9eecc378456542e3bd1566a797908ffb6
hezi-yiquanchaoren <2297142423@qq.com>
2019-08-20 18:53:23 +0800

    Exception

---
+++ 11-springboot-Exception5/src/main/java/com/hezi/controller/GlobleException.java
@@ -0,0 +1,34 @@
+package com.hezi.controller;
+
+import java.util.Properties;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+@Configuration
+public class GlobleException implements HandlerExceptionResolver{
+
+	@Override
+	public ModelAndView resolveException(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2,
+			Exception e) {
+		ModelAndView mv=new ModelAndView();
+		if(e instanceof ArithmeticException) {
+			mv.setViewName("error1");
+		}
+		if(e instanceof NullPointerException) {
+			mv.setViewName("error2");
+		}
+		
+		  mv.addObject("mv",e.toString());
+		return mv;
+	}
+	
+	
+	
+}
