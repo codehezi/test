9d5ab8847cabf898ff362c50e62d6b6d54ca6170
hezi-yiquanchaoren <2297142423@qq.com>
2019-09-02 14:19:21 +0800

    14-springboot-devtools

---
+++ 14-springboot-devtools/src/main/java/com/hezi/controller/addUser.java
@@ -0,0 +1,13 @@
+package com.hezi.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class addUser {
+	@RequestMapping("/add")
+	public String show() {
+		System.out.println("hi....ggg..qq");
+		return "list";
+	}
+} 
